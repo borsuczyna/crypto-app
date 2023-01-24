5e33bf3b23589f31e0820a2f6aff5640c233aecb
borsuczyna <libxxerty@gmail.com>
2023-01-24 19:37:30 +0100

    api fix

--- src/services/info.ts
+++ src/services/info.ts
@@ -5,7 +5,7 @@ export async function getCurrencyInfo(id: number): Promise<Currency> {
     let result = await fetch(uris.info.replace('<id>', id+''), {
         method: 'GET',
         headers: {
-            'X-CMC_PRO_API_KEY': '0f18e676-e4b7-4e71-8fd0-32a77ef5ab5d',
+            'X-CMC_PRO_API_KEY': uris.apiKey,
         }
     });
     if(result.status != 200) {
